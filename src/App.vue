26c9d5227a6dd63f2cc48fb879944489d253f2e4
RubyOzgeWu <rubyozgewu@gmail.com>
2022-07-18 09:34:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+v-app
+  router-view
+</template>
+
+<script setup>
+</script>
+
+<script setup>
+import { useUserStore } from '@/stores/user'
+useUserStore().getUser()
+</script>
